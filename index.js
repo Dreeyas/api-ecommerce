569325dda77251f85dc3fd0715bc0eda332d6ea4
Dreeyas <a.navarretegarrido@gmail.com>
2023-11-20 16:36:53 -0300

    index.js fix

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const app = express()
-app.use(express.json())
+const app = express();
 
+app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI)
-.then(() => {
+  .then(() => {
     app.listen(process.env.PORT, () => {
-        console.log('Server started on port ' + process.env.PORT)
-    })
-}).catch((error) => {
-    console.log(error)
-})
+      console.log('Server started on port ' + process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
 
-const productRoute = require ('./routes/Product.router');
-app.use(express.json())
-app.use('/api/product', productRoute)
+const productRoute = require('./routes/Product.router');
+app.use('/api/product', productRoute);
